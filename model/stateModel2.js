5228e153f9ddb628346e17b62833859ff520db20
Eric Wolf <coyocanid@gmail.com>
2026-01-05 16:30:17 -0800

    redezux with sat data

--- model/stateModel2.js
+++
@@ -1,8 +0,0 @@
-// Alias wrapper to preserve existing imports that expect stateModel2
-const stateModel = require('./stateModel.js');
-
-module.exports = stateModel;
-
-if (typeof window !== 'undefined') {
-  window.stateModel2 = stateModel;
-}
